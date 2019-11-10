23ce2c0e71ec02c0211654b5f59c61858e56153a
Daniel Stewart <des253@hotmail.com>
2019-11-09 22:09:23 -0500

    Create Project

---
+++ FlipBit/FlipBit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlipBit
+//
+//  Created by Daniel Stewart on 11/9/19.
+//  Copyright © 2019 DS Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
