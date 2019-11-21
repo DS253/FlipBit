2cbb1d344244f5d355dcd0bcf68883737e80619d
Daniel Stewart <daniel.stewart1@alaskaair.com>
2019-11-21 14:56:27 -0800

    Create BybitTradeVC

---
+++ FlipBit/FlipBit/Controllers/Bybit/BybitTradeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BybitTradeViewController.swift
+//  FlipBit
+//
+//  Created by Daniel Stewart on 11/21/19.
+//  Copyright © 2019 DS Studios. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BybitTradeViewController: BaseTableViewController {
+    
+}
