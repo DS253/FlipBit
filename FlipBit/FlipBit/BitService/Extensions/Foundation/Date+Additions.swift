28f87f82328c70439bab6a6dd195c74266eee1b5
Daniel Stewart <des253@hotmail.com>
2020-02-10 21:34:56 -0800

    Add Chart Date Formatting

--- FlipBit/FlipBit/BitService/Extensions/Foundation/Date+Additions.swift
+++ FlipBit/FlipBit/BitService/Extensions/Foundation/Date+Additions.swift
@@ -12,26 +12,8 @@ internal extension Date {
     
     /// Provides specified date time formats
     enum Format: String {
-        /// Represents "MMM dd, yyyy"
-        case shortMonthDayYear = "MMM dd, yyyy"
-        /// Represents "yyyy-MM-dd'T'HH:mm:ss"
         case bybitDateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        /// Represents "d MMM yyyy"
-        case dayShortMonthYear = "d MMM yyyy"
-        /// Represents "dd-MM-yyyy"
-        case dayMonthYear = "dd-MM-yyyy"
-        /// Represents "dd MMM yyyy"
-        case dayMonthNameYear = "dd MMM yyyy"
-        /// Represents "yyyy-MM-dd"
-        case yearMonthDay = "yyyy-MM-dd"
-        /// Represent "MM-dd-yyyy"
-        case monthDayYear = "MM-dd-yyyy"
-        /// Represents "h:mm a"
-        case twelveHourTime = "h:mm a"
-        /// Represents "MMM dd"
-        case shortMonthDay = "MMM dd"
-        /// Represents "MMMM d"
-        case longMonthShortDay = "MMMM d"
+        case chartFormat = "H:mm a, MMM d"
     }
     
         /// Provides a default date formatter for use with dates from services
@@ -78,31 +60,6 @@ internal extension Date {
         return Calendar.current.date(from: dateComponents)
     }
 
-    /// Returns true if current date is "today".
-    var isToday: Bool {
-        return self.day == Date().day
-    }
-
-    /// Returns true if current date is "yesterday".
-    var isYesterday: Bool {
-        guard let today = Date().day else { return false }
-
-        var dateComponents = DateComponents()
-        dateComponents.setValue(-1, for: .day)
-        let yesterday = Date.utcCalendar.date(byAdding: dateComponents, to: today)
-        return self.day == yesterday
-    }
-
-    /// Returns true if current date is "tomorrow".
-    var isTomorrow: Bool {
-        guard let today = Date().day else { return false }
-
-        var dateComponents = DateComponents()
-        dateComponents.setValue(+1, for: .day)
-        let tomorrow = Date.utcCalendar.date(byAdding: dateComponents, to: today)
-        return self.day == tomorrow
-    }
-
     /// Inits from a string using our global date formatter.
     init?(from string: String, format: Date.Format = .bybitDateFormat) {
 
@@ -117,37 +74,11 @@ internal extension Date {
             return nil
         }
     }
-    
-    /// Returns a date with seconds cleared or set to zero.
-    func clearSeconds() -> Date? {
-        let dateComponents = Date.utcCalendar.dateComponents([.year, .month, .day, .hour, .minute], from: self)
-        return Date.utcCalendar.date(from: dateComponents)
-    }
 
     /// Returns a string of the date in the specified format
     func string(_ format: Format) -> String {
         let formatter = Date.formatter
         formatter.dateFormat = format.rawValue
-        return formatter.string(from: self)
-    }
-    
-    /// Returns date as a string in standard format.
-    var standardString: String {
-        let formatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .iso8601)
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-
-        formatter.dateFormat = "yyyy-MM-dd"
-
-        return formatter.string(from: self)
-    }
-    
-    /// Returns date as a string in the format "H:MM:ss"
-    var hourlyFormatString: String {
-        let formatter = Date.formatter
-        formatter.timeZone = .current
-        formatter.dateFormat = "H:mm:ss"
         return formatter.string(from: self)
     }
     
